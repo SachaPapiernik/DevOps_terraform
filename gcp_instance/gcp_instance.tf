cfdf42f0ca23e8535342377793220ca86997ce3b
SachaPapiernik <sacha.papiernik@gmail.com>
2024-04-24 11:51:41 +0200

    updated gcp_instance

--- gcp_instance/gcp_instance.tf
+++ gcp_instance/gcp_instance.tf
@@ -1,7 +1,7 @@
 provider "google" {
   credentials = file("terraform-421308-4d7d9e5c0fc3.json")
-  project = "terraform-421308"
-  region  = "europe-west1"
+  project     = "terraform-421308"
+  region      = "europe-west1"
 }
 
 resource "google_compute_address" "static_ip" {
@@ -9,21 +9,64 @@ resource "google_compute_address" "static_ip" {
   region = "europe-west1"
 }
 
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+
+}
+
 resource "google_compute_instance" "vm_instance" {
-  name         = "my-instance"
+  name         = "my-instance2"
   machine_type = "e2-medium"
   zone         = "europe-west1-b"
+    
+    boot_disk {
+        auto_delete = true
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-10"
+        initialize_params {
+            image = "debian-cloud/debian-10"
+        }
     }
-  }
 
-  network_interface {
+    network_interface {
+        network = "default"
+        access_config {
+            nat_ip = google_compute_address.static_ip.address
+        }
+    }
+
+    metadata = {
+        sshKeys = "debian:${tls_private_key.ssh.public_key_openssh}"
+    }
+}
+
+resource "google_compute_firewall" "demo-ssh-ipv4" {
+    name    = "staging-demo-ssh-ipv4"
     network = "default"
-    access_config {
-      nat_ip = google_compute_address.static_ip.address
+
+    allow {
+        protocol = "tcp"
+        ports    = [22]
     }
-  }
+
+    allow {
+        protocol = "udp"
+        ports    = [22]
+    }
+
+    allow {
+        protocol = "sctp"
+        ports    = [22]
+    }
+
+    source_ranges = ["0.0.0.0/0"]
+}
+
+resource "local_file" "local_ssh_key" {
+    content  = tls_private_key.ssh.private_key_pem
+    filename = "${path.root}/ssh-keys/ssh_key"
+}
+
+resource "local_file" "local_ssh_key_pub" {
+    content  = tls_private_key.ssh.public_key_openssh
+    filename = "${path.root}/ssh-keys/ssh_key.pub"
 }
